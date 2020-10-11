84012f51dcf0f224c509c91322c98e9fdc80153d
Markus-cpu <60017889+Markus-cpu@users.noreply.github.com>
2020-10-11 22:36:23 +0300

    'Youtube popups, and deploy on firebase'

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -1,12 +1,15 @@
 import React, {useEffect, useState} from "react";
 import "./Row.css";
 import instance from "../../axios";
+import YouTube from "react-youtube";
+import movieTrailer from "movie-trailer";
 // import posterImg from "./../../images/poster.jpg";
 
 const base_url = 'https://image.tmdb.org/t/p/original/'
 
 const Row = ({ title, fetchUrl, isLargeRow }) => {
     const [movies, setMovies] = useState([])
+    const [trailerUrl, setTrailerUrl] = useState('')
 
     useEffect(() => {
         async function fetchData() {
@@ -16,7 +19,26 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
         }
         fetchData()
     }, [fetchUrl])
-    console.log(movies)
+    const opts = {
+        height: '390',
+        width: '100%',
+        playerVars: {
+            // https://developers.google.com/youtube/player_parameters
+            autoplay: 1,
+        },
+    }
+    const handleClick = (movie) => {
+        if(trailerUrl) {
+            setTrailerUrl('')
+        } else {
+            movieTrailer(movie?.name || '')
+                .then(url => {
+                    const urlParams = new URLSearchParams(new URL(url).search)
+                    setTrailerUrl(urlParams.get('v'))
+                })
+                .catch((error) => console.log(error))
+        }
+    }
     return (
         <div className="row">
             <h2>{title}</h2>
@@ -24,6 +46,7 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
                 {movies.map(movie => (
                     <img
                         key={movie.id}
+                        onClick={() => handleClick(movie)}
                         className={`row__poster ${isLargeRow && "row__posterLarge"}`}
                         src={`${base_url}${isLargeRow 
                             ? movie.poster_path 
@@ -33,6 +56,7 @@ const Row = ({ title, fetchUrl, isLargeRow }) => {
                     />
                 ))}
             </div>
+            {trailerUrl && <YouTube videoId={trailerUrl} opts={opts}/>}
         </div>
     )
 }
